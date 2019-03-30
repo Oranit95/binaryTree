1fcf0ac3449c5b14a3e5eb90359d7481d5f80395
Oranit Cohen <44542433+Oranit95@users.noreply.github.com>
2019-03-30 15:38:27 +0300

    Update Tree.cpp

--- Tree.cpp
+++ Tree.cpp
@@ -31,17 +31,19 @@ void Tree::deleteTree(node* Troot){
 //inserts i to the tree in thr right place, returns true if succeeded
 void Tree::insert(int i){
 
+//if i is already in the tree, throw an exception
 if(contains(i)==true){
 	throw std::invalid_argument( "number already exist in the tree!" );
 	}
-else{		
-	Tsize++;		
-
+else{	
+	
+	Tsize++;	
+	//if the tree is not empty, need another function to check where to put i	
 	if(Troot != NULL){
 
 		insert2(i, Troot);
         }
-	
+	//if the tree is empty
 	else{
 		Troot = new node;
 		Troot->data = i;
@@ -53,13 +55,15 @@ else{
 }
 }
 
+//insert for the case the tree is not empty
 void Tree::insert2(int i, node *Troot){
-
+	//searching int the left side
 	if(i < Troot->data){
+		//if we already have a left child, need to search again
 		if(Troot->left != NULL){
 			insert2(i, Troot->left);
 
-		}else{
+		}else{//no left child, just put it in
 			Troot->left = new node;
 			Troot->left->data = i;
 			Troot->left->left = NULL;
@@ -67,6 +71,7 @@ void Tree::insert2(int i, node *Troot){
                         Troot->left->parent = Troot;
 
 		}
+	//same with the right side
 	}else if(i > Troot->data){
 		if(Troot->right != NULL){
 			insert2(i, Troot->right);
@@ -81,7 +86,7 @@ void Tree::insert2(int i, node *Troot){
 	}
 	
 }
-
+//binary search to know if an i is in the tree or not
 bool Tree::search(int i, node* Troot){
 
 	if(Troot != NULL){
@@ -97,7 +102,7 @@ bool Tree::search(int i, node* Troot){
 		return false;
 	}
 }
-
+//bynarty search to return the node if it's in the tree
 node * Tree::search2(int i, node *Troot){
 
 if (Troot==NULL) return NULL;
@@ -121,7 +126,7 @@ bool Tree::contains(int i){
 	//returns the number that's in the root of the tree
 int Tree::root()
 {
-if(Troot==NULL)  throw std::invalid_argument( "NULL POINTER EXCEPTION" );
+	if(Troot==NULL)  throw std::invalid_argument( "NULL POINTER EXCEPTION" );
 	return Troot->data;
 }
 
@@ -132,7 +137,7 @@ int Tree::left(int i)
 	   throw std::invalid_argument( "NULL POINTER EXCEPTION" ); 
 	}
 	node* temp = Troot;
-	while(temp!=NULL){
+	while(temp!=NULL){//searching all over the tree to find it
 		if (temp->data>i) temp=temp->left;
 		else if(temp->data<i) temp=temp->right;
 		else {
@@ -151,7 +156,7 @@ int Tree::right(int i)
 	   throw std::invalid_argument( "NULL POINTER EXCEPTION" ); 
 	}
 	node* temp = Troot;
-	while(temp!=NULL){
+	while(temp!=NULL){//searching all over the tree to find it
 		if (temp->data>i) temp=temp->left;
 		else if(temp->data<i) temp=temp->right;
 		else {
@@ -166,19 +171,19 @@ int Tree::right(int i)
 int Tree::parent (int i)
 {
 
-node *temp = search2(i, Troot);
-if(temp == NULL || Troot->data==i)
+node *temp = search2(i, Troot);//searching the right i
+if(temp == NULL || Troot->data==i)//checking nothing's not null
 throw std::invalid_argument( "NULL POINTER EXCEPTION" );
 return temp->parent->data;
 
 } 
 
- 	//prints the structure of the tree
+ 	//prints the structure of the tree in order
 void Tree::print()
 {
-print2(Troot);
+	print2(Troot);
 }
-void Tree::print2(node *Troot){
+void Tree::print2(node *Troot){//helper to print function
 	if(Troot != NULL){
 		print2(Troot->left);
 		cout << Troot->data << ",";
@@ -186,7 +191,7 @@ void Tree::print2(node *Troot){
 	}
 }
 
-node* Tree::FindMax(node* Troot)
+node* Tree::FindMax(node* Troot)//helper to the remove, in order to find out hoe to change the node's location
 {
     if(Troot==NULL)
     return NULL;
@@ -198,14 +203,15 @@ node* Tree::FindMax(node* Troot)
     return Troot;
 }
 
-node* Tree::DeleteNodeInBST(node* Troot,int i)
+node* Tree::DeleteNodeInBST(node* Troot,int i)//delete the node with i
 {
-
+	//seraching for thr right node to delete
     if(Troot==NULL) return NULL;
     if(i<Troot->data && Troot->left!=NULL) 
         Troot->left = DeleteNodeInBST(Troot->left, i);
     else if (i > Troot->data && Troot->right!=NULL)
         Troot->right = DeleteNodeInBST(Troot->right, i);
+//found it
     else
     {
         //No child
@@ -218,20 +224,20 @@ node* Tree::DeleteNodeInBST(node* Troot,int i)
         else if(Troot->right == NULL)
         {
             node* temp = Troot->left;
-            if(temp != NULL) temp->parent = Troot->parent;
+	    if(temp != NULL) temp->parent = Troot->parent;
             delete(Troot);
             Troot = NULL; 
 	    return(temp);
         }
         else if(Troot->left == NULL)
         {
-            node* temp = Troot->right;     
+            node* temp = Troot->right;
 	    if(temp != NULL) temp->parent = Troot->parent;
             delete(Troot);
             Troot = NULL; 
 	    return(temp);
         }
-        //two child
+        //two child - need to check what node will replace the deleted one
         else
         {
             node* temp = FindMax(Troot->left);
@@ -245,15 +251,11 @@ node* Tree::DeleteNodeInBST(node* Troot,int i)
 void Tree::remove(int i)
 {
 
-
-node *temp1 = search2(i, Troot);
-if (temp1 == NULL ||contains(i) == false) throw std::invalid_argument ("This node doesnt exist in the tree");
-node *temp2 = DeleteNodeInBST(Troot, i);
-if (temp1 == Troot) Troot = temp2;
-if (temp2 == NULL) Troot = NULL;
-
-
-
-Tsize--;
+	node *temp1 = search2(i, Troot);//searching the right node
+	if (temp1 == NULL ||contains(i) == false) throw std::invalid_argument ("This node doesnt exist in the tree");
+	node *temp2 = DeleteNodeInBST(Troot, i);
+	if (temp1 == Troot) Troot = temp2;//updates the root
+	if (temp2 == NULL) Troot = NULL;//if it is the root
+	Tsize--;
 
 }
